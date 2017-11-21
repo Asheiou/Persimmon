b93dff502bb3f82f51442e6462d5f3a1b6af45a5
iiSkyii <31135263+iiSkyii@users.noreply.github.com>
2017-11-21 21:40:18 +0000

    v1.1

--- Persimmon.py
+++ Persimmon.py
@@ -7,66 +7,63 @@ def isInt(s):
 def restart():
     time.sleep(1)
     print("""
-
     =======+++=======
           ERROR:
      ENTER A CORRECT
           VALUE!
       RESTARTING...
     =======+++=======
-
         """)
     time.sleep(3)
     restart = 1
-    while restart == 1:
-        restart = 0
-        continue
 
 while True:
     import time, random
     print("""
-
     =======+++=======
         WELCOME TO
         PERSIMMON
     =======+++=======
-
-
     """)
     time.sleep(2)
-    difficulty = input("SELECT YOUR DIFFICULTY: TYPE 'EASY' OR 'HARD' ")
+    difficulty = input("SELECT YOUR DIFFICULTY: TYPE 'EASY' OR 'HARD', or 'HELP' for more options ")
     time.sleep(2)
     if difficulty == "EASY":
         maxn = 5
     elif difficulty == "HARD":
         maxn = 10
-    else:
-        restart()
-
-    rules = input("Would you like to read the rules? Y/N ")
-    if rules == "Y":
-        print("""RULES:
+    elif difficulty == "HELP":
+        what = input("""Welcome to the help menu.
+1. Rules
+2. Highscore""")
+        if what == "1":
+            print("""RULES:
 A number will be randomly selected each round.
 Guess the number correctly to get 10 points.
 If you don't guess correctly, not to worry, but you will lose 1 point.
-You are guessing between 1 and""",str(maxn) + """.
 Good luck!""")
-        
-    elif rules == "N":
-        pass
-    
-    else:
-        restart()
-    
-    time.sleep(2)
-    rounds = input("How many rounds would you like to play?")
+            time.sleep(6)
+            continue
+        elif what == "2":
+            hisc = open("highscore.txt")
+            print("HIGHSCORE =",hisc.read())
+            time.sleep(5)
+            
+        else:
+            restart()
+            continue
+
+
+    rounds = input("How many rounds would you like to play? ")
     
     if isInt(rounds) == True:
         rounds = int(rounds)
         if rounds <= 0:
             restart()
+            continue
     else:
         restart()
+        continue
     
     score = 0
                  
@@ -83,4 +80,20 @@ Good luck!""")
 
     time.sleep(2)
     print("GAME OVER! You scored",str(score) + "!")
-    time.sleep(2)
+
+    highscore = 0
+
+with open("highscore.txt", "r+") as hisc:
+    hi = hisc.read()
+    if not hi:  
+        hi = '0'
+    if score > int(hi):
+        print("NEW HIGHSCORE!")
+        hisc.seek(0)  
+        hisc.write(str(score))
+    else:
+        print("HIGHSCORE =%s" % hi)
+
+    time.sleep(3)
+        
+
